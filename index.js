69bcd16c480f2b047779e397933280e93a482840
Monse0051 <rouge0051@gmail.com>
2019-08-05 12:55:20 -0500

    Adding libraries and script for payment processing

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+// body-parser configuration
+// FIXME: warning: body-parser deprecated undefined extended: provide extended option app.js:5:20
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// When deploy change port to 8080
 const port = 8080;
 
-app.get('/', (req, res) => res.send({token: 123, auth: 1234}));
-app.get('/pay', (req, res)=> res.send("pay REST"));
+const Payment = require('./pay');
+const payment = new Payment();
 
-app.listen(process.env.PORT  || 8080);
+let userData = {
+    "amount": "5000.00",
+    "currency": "USD",
+    "card": {
+      "nameOnCard": "Christopher",
+      "cardNumber": "4242424242424242",
+      "expirationMonth": "03",
+      "expirationYear": "2020",
+      "securityCode": "123",
+      "billingAddress": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    },
+    "payer": {
+      "name": "Christina C",
+      "email": "cchen+test@paystand.com",
+      "address": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    }
+  };
+
+app.get('/', (req, res) => res.send({token: 123, auth: 1234}));
+
+app.post('/pay', (req, res)=> {
+    console.log("req:");
+    console.log(req.body);
+    payment.setUserData(req.body);
+
+    payment.pay().then(result => {
+        res.json({ result });
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ pay.js
@@ -0,0 +1,198 @@
+const requestPromise = require('request-promise-native');
+
+
+class Payment {
+  constructor() {
+    this.data = null;
+    this.URL_AUTH = 'https://api.paystand.co/v3/oauth/token';
+    this.URL_PAYMENTS = 'https://api.paystand.co/v3/payments/secure';
+
+  }
+
+  /**
+   * set creadict card data 
+   * @param {object} data: contain the user billing information 
+   */
+  setUserData(data) {
+    this.data = data;
+  }
+
+  /**
+   * Private method that returns token object which includes URL, header and body.
+   * @param {string} clientId: alphanumeric id 
+   * @param {string} clientSecret: alphanumeric id
+   * @returns {object} token
+   */  
+  _createTokenOptions(clientId, clientSecret) {
+    return {
+      url: this.URL_AUTH,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: {
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: "auth"
+      },
+      json: true
+    };
+  }
+
+   /**
+   * Private method that returns token object which includes URL, header and body.
+   * @param {string} auth: authentification token  
+   * @param {string} xCustomerId: alphanumeric id
+   * @param {object} data: user billing data
+   * @returns {object} token
+   */   
+  _createPaymentOptions(auth, xCustomerId, data) {
+    return {
+      url: this.URL_PAYMENTS,
+      headers: {
+        Authorization: auth,
+        'X-CUSTOMER-ID': xCustomerId,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: data,
+      json: true
+    };
+  }
+
+  /**
+   * @returns {object} object with costumer keys
+   */ 
+  _fetchKeys() {
+    return {
+      client_id: '37c6d87c956acbb50f7502300c71dbe2',
+      client_secret: 'dbc17981950c3ded3aea051110cc05d562659209',
+      xCustomerId: 'ody8bd9st5qpq8619vj3j173'
+    };
+  }
+
+  /**
+   * send post method with a user bill data 
+   * @returns {Promise} result of POST transaction
+   */
+  _authenticate() {
+    const keys = this._fetchKeys();
+    const options = this._createTokenOptions(keys.client_id, keys.client_secret);
+
+    return requestPromise.post(options)
+      .then(tokenInfo => {
+        return tokenInfo.token_type + ' ' + tokenInfo.access_token;
+      })
+      .catch(err => {
+        console.log(err);
+        //TODO: handle error
+      });
+  }
+
+  /**
+   * get authentication token and then send payment transaction to Paystand
+   * @returns {Promise} result of POST transaction
+   */
+  pay() {
+    const keys = this._fetchKeys();
+
+    return this._authenticate()
+      .then(accessToken => {
+        const options = this._createPaymentOptions(accessToken, keys.xCustomerId, this.data);
+        return requestPromise.post(options);
+      })
+      .then(response => {
+        console.log("Pago efectuado con exito!!");
+        return true;
+      })
+      .catch(err => {
+        console.log("Error en transaccion :(");
+        return false;
+      });
+  }
+}
+
+//testing
+
+function test(params) {
+
+
+  const payment1 = new Payment();
+  const payment2 = new Payment();
+
+  let userData = {
+    "amount": "5000.00",
+    "currency": "USD",
+    "card": {
+      "nameOnCard": "Christopher",
+      "cardNumber": "4242424242424242",
+      "expirationMonth": "03",
+      "expirationYear": "2020",
+      "securityCode": "123",
+      "billingAddress": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    },
+    "payer": {
+      "name": "Christina C",
+      "email": "cchen+test@paystand.com",
+      "address": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    }
+  };
+
+  let wrongUserData = {
+    "amount": "5000.00",
+    "currency": "USD",
+    "card": {
+      "nameOnCard": "Christopher",
+      "cardNumber": "4242424242424240",
+      "expirationMonth": "03",
+      "expirationYear": "2020",
+      "securityCode": "123",
+      "billingAddress": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    },
+    "payer": {
+      "name": "Christina C",
+      "email": "cchen+test@paystand.com",
+      "address": {
+        "street1": "41 Grandview St Unit C",
+        "city": "Santa Cruz",
+        "state": "CA",
+        "postalCode": "95060",
+        "country": "USA"
+      }
+    }
+  };
+
+  console.log("Simulating correct transaction...")
+  payment1.setUserData(userData);
+  payment1.pay().then(result => console.log(result));
+
+
+  console.log("Simulating wrong transaction ...");
+  payment2.setUserData(wrongUserData);
+  payment2.pay().then(result => console.log(result));
+
+
+}
+
+//test();
+
+module.exports = Payment;
