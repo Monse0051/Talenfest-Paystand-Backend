868762ee723fab8b05f1f3ee907e1dd24e97c897
Monse0051 <rouge0051@gmail.com>
2019-08-05 11:58:10 -0500

    Change to port 8080 for local testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 8080;
 
 app.get('/', (req, res) => res.send({token: 123, auth: 1234}));
 app.get('/pay', (req, res)=> res.send("pay REST"));
